c9b3b984c599db9748bec4976216d7ad136c3920
kantasantosh <kantasantosh@gmail.com>
2026-06-22 15:58:30 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'emi-calc-v1';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE_NAME = 'emi-calc-v2';
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
